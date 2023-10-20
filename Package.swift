7fd3f3c5e62fb054ff6c69314bdeefd5e5e601a3
Mahmoud Omara <mahmoud.omara909@gmail.com>
2023-10-20 19:36:07 +0300

    Add package sources

--- Package.swift
+++ Package.swift
@@ -1,28 +1,31 @@
 // swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// 1
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of
+// Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
+  // 2
+  name: "PetSaveOnboarding",
+  // 3
+  platforms: [.iOS(.v15), .macOS(.v10_15)],
+  // 4
+  products: [
+    .library(
     name: "PetSaveOnboarding",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "PetSaveOnboarding",
-            targets: ["PetSaveOnboarding"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "PetSaveOnboarding",
-            dependencies: []),
-        .testTarget(
-            name: "PetSaveOnboardingTests",
-            dependencies: ["PetSaveOnboarding"]),
-    ]
+    targets: ["PetSaveOnboarding"]),
+  ],
+  // 5
+  dependencies: [],
+  // 6
+  targets: [
+    .target(
+    name: "PetSaveOnboarding",
+    resources: [.copy("Resources/Assets.xcassets")]),
+  ]
 )
+
